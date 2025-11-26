a2ee1d1fb77d847a56c0796d52d81b161d016429
duonghyhenry <duonghyhenry@gmail.com>
2025-11-26 03:40:50 -0800

    Made rudimentary pseudocode _get_obs function and __init__ function
    TODO: Implement obs into step, reset, and make sure _get_obs works and structure is sound

--- ai/rl/env/tienlen_env.py
+++ ai/rl/env/tienlen_env.py
@@ -3,31 +3,58 @@ import requests
 import numpy as np
 import gymnasium as gym
 
-# Observation space:
-# 
-# played_cards
-# agent_hand
-# last_played
-# opponent_card_count
-# opponents_passed
-# control
-# game_id
-# player_id
-
 class TienLenEnv(gym.Env):
     
     def __init__(self):
         self.played_cards = 0
+        self.server_url = "http://localhost:5000/tienlen"
+        self.player_id = "agent"
+        self.card_counting = np.zeros(52)
+        for i in range (0, 52):
+            if i == 0:
+                self.actions[i] = ("PASS", None)
+            else:
+                self.actions[i] = ("SINGLE", i-1)
+        self.agent_hand = None
+        self.last_played = None
+        self.game_id = None
+        self.players_in = None
+        self.opponent1_card_count = None
+        self.opponent2_card_count = None
+        self.opponent3_card_count = None
+        self.opponents_passed = None
+        self.current_controller = None
+        self.current_player = None
+        self.roundNumber = None
+
         
     def _get_obs(self, state):
-        return
+        self.agent_hand = state["players"][0]["hand"]
+        self.last_played = state["phase"]["lastComboPlayed"]
+        self.opponent1_card_count = state["players"][1]["hand"].length
+        self.opponent2_card_count = state["players"][2]["hand"].length
+        self.opponent3_card_count = state["players"][3]["hand"].length
+        self.current_controller = state["phase"]["controller"]
+        self.current_player = state["phase"]["currentPlayer"]
+        self.players_in = state["phase"]["playersIn"]
+        obs = [
+            self.agent_hand, 
+            self.last_played, 
+            self.opponent1_card_count,
+            self.opponent2_card_count,
+            self.opponent3_card_count,
+            self.current_controller,
+            self.current_player,
+            self.players_in
+            ]
+        return obs
     
     def _get_info(self):
         return
         
     def step(self, action):
         if action == "PASS":
-            response = requests.post('http://localhost:3000/tienlen/${game_id}/pass/${player_id}')
+            response = requests.post('${self.server_url}/${self.game_id}/pass/${self.player_id}')
             data = response.json()
             self.game_id = data["gameID"]
             state = data["state"]
@@ -37,7 +64,7 @@ class TienLenEnv(gym.Env):
                 "move": action
             }
             response = requests.post(
-                'http://localhost:3000/tienlen/${game_id}/play/${player_id}'
+                '${self.server_url}/${self.game_id}/play/${self.player_id}'
                 json = payload
             )
             data = response.json()
@@ -52,7 +79,7 @@ class TienLenEnv(gym.Env):
         }
 
         response = requests.post(
-            "http://localhost:3000/tienlen/start"
+            "${self.server_url}/start"
             json = payload
         )
 
