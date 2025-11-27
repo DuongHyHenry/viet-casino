256db7162996bd794dac4d0d509d9125acabd5a6
duonghyhenry <duonghyhenry@gmail.com>
2025-11-27 04:12:20 -0800

    Progress on env, redefined functions, _get_obs and step mostly working now
    TODO: Nitty gritty of observation and action spaces along with correct formatting of everything

--- ai/rl/env/tienlen_env.py
+++ ai/rl/env/tienlen_env.py
@@ -10,83 +10,103 @@ class TienLenEnv(gym.Env):
         self.server_url = "http://localhost:5000/tienlen"
         self.player_id = "agent"
         self.card_counting = np.zeros(52)
-        for i in range (0, 52):
+        self.action = []
+        for i in range (0, 53):
             if i == 0:
-                self.actions[i] = ("PASS", None)
+                self.action.append(("PASS", None))
             else:
-                self.actions[i] = ("SINGLE", i-1)
-        self.agent_hand = None
-        self.last_played = None
+                self.action.append(("SINGLE", i-1))
+        self.agent_hand = np.zeros(52) # Needs to be able to represent all cards in the game
+        self.last_played = np.zeros(52)
         self.game_id = None
-        self.players_in = None
-        self.opponent1_card_count = None
-        self.opponent2_card_count = None
-        self.opponent3_card_count = None
-        self.opponents_passed = None
-        self.current_controller = None
-        self.current_player = None
-        self.roundNumber = None
+        self.players_in = np.zeros(4)
+        self.opponent_card_count = np.zeros(3)
+        self.opponents_passed = np.zeros(4) # Making it all consistent, using a mask to represent all passed opponents
+        self.current_controller = np.zeros(4) 
+        self.current_player = np.zeros(4) # Making it all consistent, using a mask to represent current player
+        self.roundNumber = 0
+        self.round_status = None
 
+    def _convert_hand(self, card_ids):
+        mask = np.zeros(52)
+        for card_id in card_ids:
+            mask[card_id] = 1
+        return mask
+    
+    def _convert_table(self, player_ids):
+        mask = np.zeros(4)
+        i = 0
+        for player_id in player_ids:
+            mask[i] = 1
+            i += 1
+        return mask
         
     def _get_obs(self, state):
-        self.agent_hand = state["players"][0]["hand"]
-        self.last_played = state["phase"]["lastComboPlayed"]
-        self.opponent1_card_count = state["players"][1]["hand"].length
-        self.opponent2_card_count = state["players"][2]["hand"].length
-        self.opponent3_card_count = state["players"][3]["hand"].length
-        self.current_controller = state["phase"]["controller"]
-        self.current_player = state["phase"]["currentPlayer"]
-        self.players_in = state["phase"]["playersIn"]
+        self.agent_hand = self._convert_hand(state["players"][0]["hand"])
+        self.last_played = self._convert_hand(state["phase"]["lastComboPlayed"]["cards"])
+        self.opponent_card_count[0] = len(state["players"][1]["hand"])
+        self.opponent_card_count[1] = len(state["players"][2]["hand"])
+        self.opponent_card_count[2] = len(state["players"][3]["hand"])
+        self.current_controller = self._convert_table(state["phase"]["round"]["controller"])
+        self.current_player = self._convert_table(state["phase"]["round"]["currentPlayer"])
+        self.players_in = self._convert_table(state["phase"]["round"]["playersIn"])
+        # TODO: Figure out a way to calculate how many opponents have passed(opponents_passed) 
         obs = [
             self.agent_hand, 
             self.last_played, 
-            self.opponent1_card_count,
-            self.opponent2_card_count,
-            self.opponent3_card_count,
+            self.opponent_card_count,
             self.current_controller,
             self.current_player,
             self.players_in
             ]
         return obs
     
-    def _get_info(self):
-        return
+    # def _get_info(self, state):
+        # info = state
+        # return info
         
-    def step(self, action):
-        if action == "PASS":
-            response = requests.post('${self.server_url}/${self.game_id}/pass/${self.player_id}')
+    def step(self, action_id: int):
+        move_type, card_id = self.action[action_id]
+        if move_type == "PASS":
+            url = f"{self.server_url}/{self.game_id}/pass/{self.player_id}"
+            response = requests.post(url)
             data = response.json()
             self.game_id = data["gameID"]
             state = data["state"]
 
-        elif action == "PLAY":
-            payload = {
-                "move": action
-            }
-            response = requests.post(
-                '${self.server_url}/${self.game_id}/play/${self.player_id}'
-                json = payload
-            )
+        else:
+            url = f"{self.server_url}/{self.game_id}/play/{self.player_id}"
+            payload = {"type": "Single", "cards": [card_id]}
+            response = requests.post(url, json=payload)
             data = response.json()
             self.game_id = data["gameID"]
             state = data["state"]
+        terminated = False
+        self.round_status = data["phase"]["type"]
+        if self.round_status == "End":
+            terminated = True
+        reward = 0
+        truncated = False
         obs = self._get_obs(state)
-        return obs
+        info = state
+        return obs, reward, terminated, truncated, info
 
     def reset(self, seed: Optional[int] = None, options: Optional[dict] = None):
-        payload = {
-            "players": ["agent", "bot1", "bot2", "bot3"]
-        }
-
-        response = requests.post(
-            "${self.server_url}/start"
-            json = payload
-        )
+        url = f"{self.server_url}/start"
+        payload = {"players": ["agent", "bot1", "bot2", "bot3"]}
+        response = requests.post(url, json=payload)
 
         data = response.json()
-
+        self.card_counting = np.zeros(52)
+        self.opponent_card_count[:] = 13
+        self.opponents_passed = np.zeros(4)
+        self.current_controller = np.zeros(4)
+        self.current_player = np.zeros(4)
+        self.roundNumber = None
+        self.agent_hand = self._convert_hand(data["state"]["players"][0]["hand"])
 
         self.game_id = data["gameID"]
         state = data["state"]
         obs = self._get_obs(state)
-        return obs
+        info = state
+        return obs, info
